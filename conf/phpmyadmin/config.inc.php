83246fdb25a76091fa7d3a46b243b9f1dea481da
N1cking <nicatkapoeira@gmail.com>
2023-04-17 19:52:33 +0200

    init commoit

---
+++ conf/phpmyadmin/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+$i = 1;
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = 'yourpassword';
+
+
+
+
